4e419cf047ee44fded5ccc0c437a1f5f400c6f86
aziad1998 <aziad1998@gmail.com>
2020-04-02 17:49:41 -0400

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,33 @@
 #include <iostream>
 #include <fstream>
 
+/**
+ * @brief interpreter starting point
+ * @param argc is the count of passed arguments
+ * @param argv an array of passed arguments
+ */
 int main(int argc, char *argv[])
 {
-	int arrayLength = 1000000;
+	int arrayLength = 1000000;  // initilize array to 1 MegaCells
 
-	array mainArray(arrayLength);
+	array mainArray(arrayLength);  // create mainArray instance of array class
 	bool running = true;
 
-	if(argv[1] != NULL)
+	if(argv[1] != NULL)  // if the user passed a file as an argument to the interpreter
 	{
 		std::fstream file;
-		file.open(argv[1]);
+		file.open(argv[1]);  // open the file
 		if(file.is_open())
 		{
 			std::string line;
-			while(std::getline(file, line) && running)
+			while(std::getline(file, line) && running)  // read one line at a time
 			{
-				for(int i = 0; i <= line.length(); i++)
+				for(int i = 0; i <= line.length(); i++) // execute one char at a time
 				{
 					interpret(line[i], mainArray, &running);
 				}
 			}
-			std::cout << "\nExecution completed, press Enter to exit...";
+			std::cout << "\n\nExecution completed, press Enter to exit...";
 			std::getchar();
 		}
 		else
@@ -31,7 +36,7 @@ int main(int argc, char *argv[])
 			std::cout << "File does not exist\n";
 		}
 	}
-	else
+	else  // if no file was passed just start the interpreter shell
 	{
 		while(running)
 		{
@@ -39,7 +44,7 @@ int main(int argc, char *argv[])
 			std::string input;
 			std::getline(std::cin, input);
 			
-			for(int i = 0; i < input.length(); i++)
+			for(int i = 0; i < input.length(); i++)  // interpret one char at a time
 			{
 				interpret(input[i], mainArray, &running);
 			}
--- src/objects.cpp
+++ src/objects.cpp
@@ -3,42 +3,64 @@
 
 // The cell class
 
+/**
+ * @brief cell constructor, initilizes value of cell to zero
+ */
 cell::cell()
 {
 	(*this).value = 0;
 }
 
+/**
+ * @brief returns value of the cell
+ */
 int cell::getValue()
 {
 	return (*this).value;
 }
 
+/**
+ * @brief sets cell value
+ * @param x and integer to store in the cell
+ */
 void cell::setValue(int x)
 {
 	(*this).value = x;
 }
 
+/**
+ * @brief incremets cell value by 1
+ */
 void cell::incrementValue()
 {
-	if((*this).value == 255)
+	if((*this).value == 255)  // 8 bit cellls
 		(*this).value = 0;
 	else
 		(*this).value++;
 }
 
+/**
+ * @brief decrements cell value by 1
+ */
 void cell::decrementValue()
 {
 	if((*this).value == 0)
-		(*this).value = 255;
+		(*this).value = 255; // 8 bit cells
 	else
 		(*this).value--;
 }
 
+/**
+ * @brief set cell position in the cell array
+ */
 void cell::setPosition(int x)
 {
 	(*this).position = x;
 }
 
+/**
+ * @brief get the position of the cell in the cell array
+ */
 int cell::getPosition()
 {
 	return (*this).position;
@@ -46,6 +68,11 @@ int cell::getPosition()
 
 // The array class
 
+/**
+ * @brief array constructor
+ * @param x the number of cells in the array
+ * @param verbose is a boolean to print an message of the numberof created cells
+ */
 array::array()
 {
 	std::cerr << "Error: You must specify array length." << std::endl;
@@ -53,13 +80,13 @@ array::array()
 
 array::array(int x, bool verbose)
 {
-	(*this).capacity = x-1; // numbering starts from 0;
-	(*this).pointerPos = 0;
+	(*this).capacity = x-1; // 0 indexed;
+	(*this).pointerPos = 0; // set position to beginning
 
-	(*this).contents = new cell[x];
+	(*this).contents = new cell[x];  //creat cell array
 	for(int i = 0; i <= (*this).capacity; i++)
 	{
-		(*this).contents[i].setPosition(i);
+		(*this).contents[i].setPosition(i);  // set cells positions
 	}
 
 	if(verbose)
@@ -68,43 +95,52 @@ array::array(int x, bool verbose)
 	}	
 }
 
+/**
+ * @brief move the pointer one cell to the right
+ */
 void array::incrementPointer()
 {
-	if((*this).pointerPos + 1 <= (*this).capacity)
+	if((*this).pointerPos + 1 <= (*this).capacity)  // check for remaining capacity
 	{
 		(*this).pointerPos++;
 	}
 	else
 	{
-		int newSize = ((*this).capacity + 1)*2;
-		cell *temp = new cell[newSize];
-		for(int i = 0; i < newSize; i++)
+		int newSize = ((*this).capacity + 1)*2;  //  get twice as current array capacity
+		cell *temp = new cell[newSize];  //  new cell array of the new size
+		for(int i = 0; i < newSize; i++)  //  initilize new cells
 		{
-			if(i <= (*this).capacity)
+			if(i <= (*this).capacity)  // if still in the range of old array copy cells data
 			{
 				temp[i].setValue((*this).contents[i].getValue());
 			}
-			temp[i].setPosition(i);
+			temp[i].setPosition(i);  // set the positions of the cells
 		}
-		delete (*this).contents;
-		(*this).contents = temp;
-		(*this).capacity = newSize - 1;
-		(*this).pointerPos++;
+		delete (*this).contents;  // free old memory
+		(*this).contents = temp;  // move new data to the right variable
+		(*this).capacity = newSize - 1;  // set new capacity
+		(*this).pointerPos++;  // incremet the pointer 
 	}
 }
 
+/**
+ * @brief move the pointer one cell to the left
+ */
 void array::decrementPointer()
 {
-	if((*this).pointerPos - 1 >= 0)
+	if((*this).pointerPos - 1 >= 0)  // check if reached the bottom
 	{
 		(*this).pointerPos--;
 	}
 	else
 	{
-		std::cerr << "Error: array bottom limit reached." << std::endl;
+		std::cerr << "Error: array underflow." << std::endl;
 	}
 }
 
+/**
+ * @brief get current pointer position
+ */
 int array::getPointerPos()
 {
 	return (*this).pointerPos;
